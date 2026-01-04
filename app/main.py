47b0638f7cdbef88c897e67c3e32de911f0a6c92
Bomberjoc <Joseramon2810@outlook.com>
2026-01-04 15:48:51 -0500

    Enrutamiento correcto

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+
 from app.routes.plants import router as plants_router
 from app.routes.activity_logs import router as activity_logs_router
-
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Vivero API")
 
@@ -13,6 +14,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
 
 app.include_router(plants_router, prefix="/plants", tags=["Plants"])
 app.include_router(activity_logs_router, prefix="/activity-logs", tags=["Activity Logs"])
